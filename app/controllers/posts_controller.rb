9cb9842652f0aa3bb36cbc161ed6351eb02464fb
anukshi13 Mittal <anukshi.mittal@kellogg.northwestern.edu>
2025-02-18 22:16:29 +0000

    final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,16 +5,26 @@ class PostsController < ApplicationController
     @posts = Post.all
     # render posts/index view
   end
-
+  def 
   def new
+    
+
+
     # render posts/new view with new Post form
   end
 
-  # def create
+  def create
+  @posts = Post.new 
+    @posts["author"]= params ["author"]
+    @posts["body"]= params ["body"]
+    @posts["image"]= params ["image"]
+    @posts.save
+    redrect_to "/posts"
+
   #   # start with a new Post
   #   # assign user-entered form data to Post's columns
   #   # save Post row
   #   # redirect user
-  # end
+  end
 
 end
